83cfb57034b1df1ef25edc7c05e7244ae127300e
maprohu <maprohu@gmail.com>
2016-10-02 12:54:39 +0100

    maven modules

--- packaging/src/main/scala/toolbox6/packaging/MavenTools.scala
+++ packaging/src/main/scala/toolbox6/packaging/MavenTools.scala
@@ -97,8 +97,8 @@ object MavenTools {
              xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
       <modelVersion>4.0.0</modelVersion>
 
-      <groupId>emsa</groupId>
-      <artifactId>osgi-deployer-temp</artifactId>
+      <groupId>toolbox6</groupId>
+      <artifactId>maven-tools-temp</artifactId>
       <version>1.0.0-SNAPSHOT</version>
       {content}
     </project>

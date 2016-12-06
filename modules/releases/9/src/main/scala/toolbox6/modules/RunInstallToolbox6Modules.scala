2faa42654474f378a1297353ea0d177faf7a776e
maprohu <maprohu@gmail.com>
2016-12-06 11:30:18 +0000

    vdm2cdf repack

---
+++ modules/releases/9/src/main/scala/toolbox6/modules/RunInstallToolbox6Modules.scala
@@ -0,0 +1,32 @@
+package toolbox6.modules
+
+import java.io.File
+
+import mvnmod.builder.MavenTools
+
+
+/**
+  * Created by pappmar on 05/10/2016.
+  */
+object RunInstallToolbox6Modules {
+
+  def main(args: Array[String]): Unit = {
+
+    val root = new File("../../../../..")
+
+    MavenTools.runMaven(
+      MavenTools.pom {
+        <packaging>pom</packaging>
+        <modules>
+          <module>{root}/maven-modules</module>
+          <module>{root}/toolbox6/modules</module>
+          <module>{root}/toolbox6</module>
+        </modules>
+      },
+      "install"
+    )(_ => ())
+
+
+  }
+
+}

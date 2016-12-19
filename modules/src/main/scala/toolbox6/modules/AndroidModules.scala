1d651d9d399b31387d6262e7bd3afea9f48c1b2d
maprohu <maprohu@gmail.com>
2016-12-19 12:24:34 +0000

    android build

--- modules/src/main/scala/toolbox6/modules/AndroidModules.scala
+++ modules/src/main/scala/toolbox6/modules/AndroidModules.scala
@@ -4,25 +4,25 @@ package toolbox6.modules
 /**
   * Created by pappmar on 30/08/2016.
   */
-object AndroidModules {
-
-//  implicit val Container = SubModuleContainer(Toolbox6Modules.Root, "android")
+//object AndroidModules {
+//
+////  implicit val Container = SubModuleContainer(Toolbox6Modules.Root, "android")
+////
+////
+////  object Packaging extends ScalaModule(
+////    "packaging",
+////    Toolbox6Modules.Packaging,
+////    mvn.`org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-spi-maven:jar:2.2.2`,
+////    mvn.`org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-impl-maven:jar:2.2.2`,
+////    mvn.`com.lihaoyi:ammonite-ops_2.11:jar:0.7.7`
+////  )
+////
+////  object LibInstaller extends ScalaModule(
+////    "libinstaller",
+////    mvn.`com.google.android:android:jar:4.1.1.4`
+////  )
 //
 //
-//  object Packaging extends ScalaModule(
-//    "packaging",
-//    Toolbox6Modules.Packaging,
-//    mvn.`org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-spi-maven:jar:2.2.2`,
-//    mvn.`org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-impl-maven:jar:2.2.2`,
-//    mvn.`com.lihaoyi:ammonite-ops_2.11:jar:0.7.7`
-//  )
 //
-//  object LibInstaller extends ScalaModule(
-//    "libinstaller",
-//    mvn.`com.google.android:android:jar:4.1.1.4`
-//  )
-
-
-
-
-}
+//
+//}
--- tools/src/test/scala/toolbox6/tools/resources.scala
+++ tools/src/test/scala/toolbox6/tools/resources.scala
@@ -3,67 +3,67 @@ package toolbox6.tools
 import java.io.FileInputStream
 
 
-object TFTest {
-  def main(args: Array[String]): Unit = {
-    for {
-      f <- TF(new FileInputStream("../toolbox6/pom.xml"))
-    } {
-      println(f)
-    }
-
-
-    for {
-      r1 <- TF[String](
-        () => {
-          println("open: r1")
-          "r1"
-        },
-        x => {
-          println(s"close: ${x}")
-        }
-      )
-      r2 <- TF[String](
-        () => {
-          println("open: r2")
-          "r2"
-        },
-        x => {
-          println(s"close: ${x}")
-        }
-      )
-    } {
-      println(r2)
-    }
-
-    val r = for {
-      r1 <- TF[String](
-        () => {
-          println("open: r1")
-          "r1"
-        },
-        x => {
-          println(s"close: ${x}")
-        }
-      )
-      r2 <- TF[String](
-        () => {
-          println("open: r2")
-          "r2"
-        },
-        x => {
-          println(s"close: ${x}")
-        }
-      )
-    } yield {
-      r2
-    }
-
-    println(
-      r.extract({ x =>
-        println(s"extracting: ${x}")
-        x
-      })
-    )
-
-  }
-}
+//object TFTest {
+//  def main(args: Array[String]): Unit = {
+//    for {
+//      f <- TF(new FileInputStream("../toolbox6/pom.xml"))
+//    } {
+//      println(f)
+//    }
+//
+//
+//    for {
+//      r1 <- TF[String](
+//        () => {
+//          println("open: r1")
+//          "r1"
+//        },
+//        x => {
+//          println(s"close: ${x}")
+//        }
+//      )
+//      r2 <- TF[String](
+//        () => {
+//          println("open: r2")
+//          "r2"
+//        },
+//        x => {
+//          println(s"close: ${x}")
+//        }
+//      )
+//    } {
+//      println(r2)
+//    }
+//
+//    val r = for {
+//      r1 <- TF[String](
+//        () => {
+//          println("open: r1")
+//          "r1"
+//        },
+//        x => {
+//          println(s"close: ${x}")
+//        }
+//      )
+//      r2 <- TF[String](
+//        () => {
+//          println("open: r2")
+//          "r2"
+//        },
+//        x => {
+//          println(s"close: ${x}")
+//        }
+//      )
+//    } yield {
+//      r2
+//    }
+//
+//    println(
+//      r.extract({ x =>
+//        println(s"extracting: ${x}")
+//        x
+//      })
+//    )
+//
+//  }
+//}
